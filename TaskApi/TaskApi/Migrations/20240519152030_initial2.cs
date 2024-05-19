bfa816a6bef0685191dae5fbe7dde6ab10a0837a
JULIANA ARENAS ARIAS <julianaarenas312755@correo.itm.edu.co>
2024-05-19 10:21:21 -0500

    update migration database

---
+++ TaskApi/TaskApi/Migrations/20240519152030_initial2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskApi.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                newName: "Tasks1");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tasks_Title",
+                table: "Tasks1",
+                newName: "IX_Tasks1_Title");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsCompleted",
+                table: "Tasks1",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "Tasks1",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Tasks1",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks1",
+                table: "Tasks1",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks1",
+                table: "Tasks1");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Tasks1");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks1",
+                newName: "Tasks");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tasks1_Title",
+                table: "Tasks",
+                newName: "IX_Tasks_Title");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IsCompleted",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks",
+                column: "Id");
+        }
+    }
+}
