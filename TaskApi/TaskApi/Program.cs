853095c78d55577cf1902b7b4416170e993b1104
JULIANA ARENAS ARIAS <julianaarenas312755@correo.itm.edu.co>
2024-05-18 23:47:08 -0500

    creation Builder

--- TaskApi/TaskApi/Program.cs
+++ TaskApi/TaskApi/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TaskApi.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
