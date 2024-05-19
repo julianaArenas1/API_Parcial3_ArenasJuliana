58099e4c94b12de23cc16a0869b96baf473875d1
JULIANA ARENAS ARIAS <julianaarenas312755@correo.itm.edu.co>
2024-05-18 23:14:53 -0500

    Implementation of the Tasks table

--- TaskApi/TaskApi/DAL/Entities/Tasks.cs
+++ TaskApi/TaskApi/DAL/Entities/Tasks.cs
@@ -1,6 +1,24 @@
-﻿namespace TaskApi.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskApi.DAL.Entities
 {
-    public class Tasks
+    public class Tasks : AuditBase
     {
+        [Display(Name = "Título")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Title { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        public string Description { get; set; }
+
+        [Display(Name = "Completado")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string IsCompleted { get; set; }
+
+        [Display(Name = "Prioridad")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Priority { get; set; }
     }
 }
