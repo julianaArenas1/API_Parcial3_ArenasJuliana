74bb5ef535f961db360084b109c83a5a08fa5f8e
JULIANA ARENAS ARIAS <julianaarenas312755@correo.itm.edu.co>
2024-05-19 22:50:48 -0500

    Finally, add the missing dependency container to the program.

--- TaskApi/TaskApi/Program.cs
+++ TaskApi/TaskApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TaskApi.DAL;
+using TaskApi.Domain.Interfaces;
+using TaskApi.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<ITask1Service, Task1Service>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
